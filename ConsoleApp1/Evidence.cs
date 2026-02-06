f1c56a8a40f44e188563c838f41800b8cc49a5b4
Marek Učeň <Marek.ucen@seznam.cz>
2026-02-06 12:09:46 +0100

    Evidence

---
+++ ConsoleApp1/Evidence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Evidence
+    {
+        private List<Zvire> seznamZvirat = new List<Zvire>();
+        private int dalsiId = 1;
+
+        public void PridatZvire(string jmeno, string druh, int vek, string pohlavi)
+        {
+            Zvire nove = new Zvire(dalsiId++, jmeno, druh, vek, pohlavi);
+            seznamZvirat.Add(nove);
+            Console.WriteLine($"Zvíře {jmeno} bylo přidáno pod názvem Evidence.");
+        }
+    }
+}
